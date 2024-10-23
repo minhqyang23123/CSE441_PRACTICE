5b7ff19f5206134a10a4f621613a0744ca18b3b7
minhqyang23123 <115971067+minhqyang23123@users.noreply.github.com>
2024-10-24 00:52:32 +0700

    done

--- EX08.1/app/src/main/java/com/example/bai081/MainActivity.java
+++ EX08.1/app/src/main/java/com/example/bai081/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.bai081;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,48 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    public class SendSMSActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+    }
+    Button btnCall = findViewById(R.id.btnCall);
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        View btnCall = null;
+        btnCall.setOnClickListener(new  View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+// TODO Auto-generated method stub
+//Tạo mới một đối tượng intent
+                Intent intent1  =new  Intent(MainActivity.this,CallPhoneActivity.class);
+//Thực thi Intent1
+                startActivity(intent1);
+            }
+        });
+        Button Send_SMS;
+        View btnSend = null;
+        btnSend.setOnClickListener(new  View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+// TODO Auto-generated method stub
+//Tạo mới một đối tượng intent
+                Intent intent2 =new Intent(MainActivity.this,SendSMSActivity.class);
+//Thực thi Intent1
+                startActivity(intent2);
+            }
+        });
+
     }
 }
